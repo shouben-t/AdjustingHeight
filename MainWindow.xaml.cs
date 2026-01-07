da7f91f28b7670a0df39622f9bd916a4226f4faf
Sjoerd Houben <sjoerd.houben@tomra.com>
2026-01-07 15:27:15 +0100

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdjustHeightHybridBlazorWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool _isOpen = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (_isOpen)
+            {
+                webView.Height = 0;
+            }
+            else
+            {
+                webView.Height = 500;
+            }
+            _isOpen = !_isOpen;
+        }
+    }
+}
